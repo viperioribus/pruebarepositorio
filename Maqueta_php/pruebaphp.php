ff7cd45d9296db41d3d6044295039c2fc1bf007b
viperioribus <viperioribus@hotmail.com>
2013-06-26 10:40:03 +0200

    first commit

---
+++ Maqueta_php/pruebaphp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css">
+<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
+<link rel="stylesheet" href="jquery.mobile-1.1.0.custom.css?" />
+<link rel="stylesheet" href="jquery.mobile.theme.overrides.css" />
+</head>
+<body>
+
+<div id="Home" data-url="Home" data-role="page">
+<div data-role="header" data-theme="d">
+<h1>Mundo Apuestas</h1>
+</div>
+<?php
+
+echo "probando el php";
+
+?>
+</div>
+</body>
+</html>
